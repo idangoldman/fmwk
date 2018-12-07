c72aa742f62e93f10b0ef08e92f6e12b1c7d6a6d
Idan Goldman <idan@goldman.work>
2018-12-07 02:58:30 +0200

    update to events tests

--- src/__tests__/events.js
+++ src/__tests__/events.js
@@ -5,34 +5,80 @@ describe('Events class tested', () => {
   let element, events, mockEventFunction;
 
   beforeAll(() => {
-    document.body.innerHTML = '<button></button>';
+    document.body.innerHTML = `
+      <button>Click Me</button>
+    `;
 
+    const selector = 'button';
     mockEventFunction = jest.fn();
-    element = new Element('button');
-    events = new Events(element);
+    element = new Element(selector);
+    events = new Events(selector, element);
   });
 
-  test('should fire an onClick event', () => {
+  test('should trigger a click event', () => {
     events.on('click', mockEventFunction);
     element.click();
     expect(mockEventFunction).toBeCalled();
   });
 
-  test('should remove the onClick event', () => {
+  test('should remove the click event', () => {
     events.off('click', mockEventFunction);
     element.click();
     expect(mockEventFunction).not.toBeCalled();
   });
+
+  test('should fire 2 events, click and focus', () => {
+    events.on('click focus', mockEventFunction);
+    element.click();
+    element.focus();
+    expect(mockEventFunction.mock.calls.length).toBe(2);
+  });
+
+  test('should remove 2 events, click and focus', () => {
+    events.off('click focus', mockEventFunction);
+    element.click();
+    element.focus();
+    expect(mockEventFunction).not.toBeCalled();
+  });
+
+  test('should remove event without specifying a function', () => {
+    events.on('click', mockEventFunction);
+    events.off('click');
+    element.click();
+    expect(mockEventFunction).not.toBeCalled();
+  });
+
+  test('should remove 2 events without specifying a function', () => {
+    events.on('click focus', mockEventFunction);
+    events.off('click focus');
+    element.click();
+    element.focus();
+    expect(mockEventFunction).not.toBeCalled();
+  });
+
+  test('should remove event without specifying a type or function', () => {
+    events.on('click', mockEventFunction);
+    events.off();
+    element.click();
+    element.focus();
+    expect(mockEventFunction).not.toBeCalled();
+  });
+
+  test('should call click event only once', () => {
+    events.once('click', mockEventFunction);
+    element.click();
+    element.click();
+    expect(mockEventFunction.mock.calls.length).toBe(1);
+  });
+
+  test('should trigger a click event', () => {
+    events.on('click', mockEventFunction);
+    events.trigger('click');
+    expect(mockEventFunction).toBeCalled();
+  });
 });
 
+// errors
 // events.on('click');
-// events.on('click focus', mockEventFunction);
-// events.on('focus', mockEventFunction)
-// events.off('click focus', mockEventFunction);
-// events.off('click focus');
-// events.off('focus');
-// events.off();
-// events.trigger('click');
-// events.trigger('click focus');
 // events.once('click');
-// events.once('click', mockEventFunction);
+// events.trigger('click focus');
