7a411dfaafd53eefb4559db4b99217cb4c847650
Idan Goldman <idan@goldman.work>
2019-08-29 12:30:25 +0300

    revert dom events code

--- src/events/index.js
+++ src/events/index.js
@@ -1,34 +1,26 @@
-import Transmitter from '../helpers/transmitter';
+import { insert, remove } from './store';
 
-const DOM_EVENTS_LIST = [
-  'blur', 'change', 'click', 'contextmenu', 'dblclick', 'focus', 'focusin',
-  'focusout', 'keydown', 'keypress', 'keyup', 'mousedown', 'mouseenter',
-  'mouseleave', 'mousemove', 'mouseout', 'mouseover', 'mouseup', 'resize',
-  'scroll', 'select', 'submit', 'reset'
-];
-
-export default class Events extends Transmitter {
+export default class Events {
   constructor(element) {
-    super(DOM_EVENTS_LIST);
     this.element = element;
   }
 
   on(name, callback) {
-    this.insert(this.element.toString, name, callback).forEach(([type, listener]) => {
+    insert(this.element.toString, name, callback).forEach(([type, listener]) => {
       this.element.raw.addEventListener(type, listener, false);
     });
   }
 
   off(name, callback) {
-    this.remove(this.element.toString, name, callback).forEach(([type, listener]) => {
+    remove(this.element.toString, name, callback).forEach(([type, listener]) => {
       this.element.raw.removeEventListener(type, listener, false);
     });
   }
 
   once(name, callback) {
-    this.insert(this.element.toString, name, callback).forEach(([type, listener]) => {
+    insert(this.element.toString, name, callback).forEach(([type, listener]) => {
       this.element.raw.addEventListener(type, (event) => {
-        this.remove(this.element.toString, type, listener);
+        remove(this.element.toString, type, listener);
         listener(event);
       }, { once: true, capture: false });
     });
