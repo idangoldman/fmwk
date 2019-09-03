6dd7f7fe7142b41800b2bd66cfb0e7e71679173c
Idan Goldman <idan@goldman.work>
2019-09-03 18:34:19 +0300

    eventNamesValidation function got fancy with doco, tests, and a small refactoring

--- src/helpers/event-names-validation.js
+++ src/helpers/event-names-validation.js
@@ -1,12 +1,25 @@
-const eventNamesValidation = (eventName = '', eventsList = []) => {
-  const names = eventName.split(' ');
-  const list = eventsList.flat(Infinity);
+/**
+ * Match event names against given event names list.
+ * @param  {String}      eventNames     String of event names separated by space.
+ * @param  {EventsList}  eventNamesList Nested array of event names.
+ * @return {Array}       Array of valid event names.
+ * @throws When event name is not part of event names list.
+ *
+ * @example
+ * const eventNames = 'click mouseover';
+ *
+ * eventNamesValidation(eventNames, DOM_EVENTS_LIST);
+ * // ['click', 'mouseover']
+ */
+const eventNamesValidation = (eventNames = '', eventNamesList = []) => {
+  const names = eventNames.split(' ');
+  const list = eventNamesList.flat(3);
 
-  names.every(name => {
+  for (const name of names) {
     if (!list.includes(name)) {
       throw new Error(`- No event was found with '${name}' name.`);
     }
-  });
+  }
 
   return names;
 };
