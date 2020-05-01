28862ca7138057e116699baf8bd6bb08f84097be
Idan Goldman <idan@goldman.work>
2020-05-02 02:58:37 +0300

    last commit of the day.

--- public/script.js
+++ public/script.js
@@ -3,13 +3,13 @@ import { component } from '../src/index';
 component('#mc_embed_signup', ({ element, events }) => {
   console.log(element);
 
-  events.on('change', event => {
-    console.log(event.target);
-  });
+  // events.on('change', '#mce-LNAME', event => {
+  //   console.log(event.target);
+  // });
 
   events.on('submit', (event) => {
     event.preventDefault();
 
-    console.log('CLICKED');
+    console.log('SUBMIT !!!');
   });
 });
