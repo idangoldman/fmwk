21f296a788d3aed9343a4d202a1248e91e64011f
Idan Goldman <idan@goldman.work>
2020-05-02 01:54:06 +0300

    fixed and cleanup of the main feature file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import Element from 'element';
 import Events from 'events';
-import Storage from 'storage';
 import onDOMContentLoaded from 'dom-content-loaded';
 
-const component = (selector, callback) => {
+export const component = (selector, callback) => {
   onDOMContentLoaded(() => {
     const element = new Element(selector);
     const events = new Events(element);
-    const local = new Storage('local');
 
-    callback({ element, events, local });
+    callback({ element, events });
   });
 };
-
-export default { component };
