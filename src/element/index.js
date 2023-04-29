b7232048c7e99132bce80f33862a7803f76da4bb
Idan Goldman <idan@123code.co>
2023-04-29 03:18:22 +0300

    remove jsdocs code comments

--- src/element/index.js
+++ src/element/index.js
@@ -1,26 +1,6 @@
-/**
-* Wrapper class for DOM elements, initiate by passing a selector.
-*/
-class Element {
-  /**
-  * Creates an element instance of the query selector passed in.
-  * @param {String} selector DOM query selector
-  * @throws When no query selector passed.
-  * @throws When no HTML elements were found via passed query selector.
-  */
+export default class Element {
   constructor(selector = '') {
-    /**
-     * An instance of the DOM element.
-     * @member {HTMLElement}
-     * @private
-     */
     this.element = undefined;
-
-    /**
-     * An instance of the query selector string.
-     * @member {QuerySelector}
-     * @private
-     */
     this.selector = selector;
 
     if (!selector.length) {
@@ -34,21 +14,11 @@ class Element {
     }
   }
 
-  /**
-   * Get the instance of DOM element.
-   * @returns {HTMLElement}
-   */
   get raw() {
     return this.element;
   }
 
-  /**
-   * Get the query selector string.
-   * @returns {QuerySelector}
-   */
   get toString() {
     return this.selector;
   }
 }
-
-export default Element;
--- src/helpers/find-reverse-branch.js
+++ src/helpers/find-reverse-branch.js
@@ -1,23 +1,3 @@
-/**
- * @description
- * Searches an {@link EventsList} array for a specific item and returns the trail from it.
- *
- * @param  {String}     leaf  String value to be found in the array
- * @param  {EventsList} tree  Nested array of string values.
- * @return {Array}            Flat array that indicates the path to the leaf in reverse.
- *
- * @example
- *
- * const tree = [['branch_1', ['leaf_1_1', 'leaf_1_2']], ['branch_2', ['leaf_2_1']], 'leaf_0'];
- * const leaf = 'leaf_1_2';
- *
- * console.log(
- *   'The branch is:', findReverseBranch(leaf, tree)
- * );
- *
- * // ['leaf_1_2', 'branch_1']
- */
-
 const findReverseBranch = (leaf = '', tree = []) => {
   const newBranch = [];
 
