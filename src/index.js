b2f5850fe71291f97595b09b66a2728242a9e6b2
Idan Goldman <idan@goldman.work>
2023-12-09 14:11:53 +0200

    it's loading well, now to writting tests and features

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Events from '~/src/events/index.js'
 import Storage from '~/src/stores/storage.js'
 import onDOMContentLoaded from '~/src/dom-content-loaded.js'
 
-const component = (selector, callback) => {
+export const component = (selector, callback) => {
   onDOMContentLoaded(() => {
     const element = new Element(selector)
     const events = new Events(element)
